39cf416962cc926be9c849b527c14126fba59c50
Nmsrt <neo.monserrat@gmail.com>
2026-04-28 20:09:32 +0800

    Split css and data files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
+
+import './styles/globals.css';
+import './styles/navbar.css';
+import './styles/hero.css';
+import './styles/experience.css';
+import './styles/tech.css';
+import './styles/projects.css';
+import './styles/contact.css';
+import './styles/footer.css';
+import './styles/cards.css';
+import './styles/responsive.css';
+import './styles/transitions.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
